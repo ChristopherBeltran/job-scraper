d61a761e3191dd194a39de3bd798cc433f06db13
Christopher Beltran <christod.beltran@gmail.com>
2020-06-04 14:31:06 -0600

    Removing models and routers, stubbing out basic api requests for each provider

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const requestIp = require('request-ip');
 require("./mongoose");
-const userRouter = require("./src/routers/user");
 
 const app = express();
 
+app.use(requestIp.mw())
 app.use(express.json());
-app.use(userRouter);
+
+app.get('/api/v1/linkedIn', async (req, res) => {
+    let baseUrl = 'https://api.linkedin.com/v1/job-search'
+    try {
+        //make call to linkedIn API
+        res.send('Linked in stuff happening soon!')
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.get('/api/v1/indeed', async (req, res) => {
+    let baseUrl = `https://api.indeed.com/ads/apisearch?publisher=${process.env.INDEED_KEY}`
+    try {
+        //make call to linkedIn API
+        res.send('Indeed stuff happening soon!')
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+app.get('/api/v1/dice', async (req, res) => {
+    let baseUrl = 'https://api.dice.com/jobs'
+    try {
+        //make call to linkedIn API
+        res.send('Dice stuff happening soon!')
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 module.exports = app;
