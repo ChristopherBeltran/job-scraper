d74c3654a35c921b14cf03bfc8bd96869ead91cb
Christopher Beltran <christod.beltran@gmail.com>
2020-06-08 11:01:33 -0600

    Fixes for Dice scraper

--- automation_scripts/dice.js
+++ automation_scripts/dice.js
@@ -11,7 +11,9 @@ const runDice = async ( /*searchParams*/ ) => {
 
     const url = 'https://www.dice.com/'
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
     const page = await browser.newPage();
     await page.goto(url);
 
@@ -29,20 +31,40 @@ const runDice = async ( /*searchParams*/ ) => {
         }),
         page.click('#submitSearch-button'),
     ]);
+    const pastThreeDays = '#facets > dhi-accordion.facet-group.ng-tns-c74-5.ng-star-inserted > div.facet-body.ng-tns-c74-5.ng-trigger.ng-trigger-expand > div > js-single-select-filter > div > div > button:nth-child(3)'
+    const pastSevenDays = '#facets > dhi-accordion.facet-group.ng-tns-c74-5.ng-star-inserted > div.facet-body.ng-tns-c74-5.ng-trigger.ng-trigger-expand > div > js-single-select-filter > div > div > button:nth-child(4)'
+
+    await page.waitFor(6000)
 
     const content = await page.content();
     const $ = cheerio.load(content);
 
     const jobs = []
-    $('div[class=".card-header"]').find('div > div > div > div > h5 > a').each(function (index, element) {
-        jobs.push(element)
-    })
+
+    $('.card-header').each(function (i, el) {
+        const titleAndLinkElement = $(el).find('.card-title-link')
+        const link = $(titleAndLinkElement).attr('href')
+        const position = $(titleAndLinkElement).text()
+        const companyElement = $(el).find('.card-company')
+        const companyChildElement = $(companyElement).find('a')
+        const co = $(companyChildElement).text()
+        const locationElement = $(companyElement).find('#searchResultLocation')
+        const location = $(locationElement).text()
+
+        jobs.push(`${position}, ${co}, ${location}, ${link}`)
+    });
+    for (i = 0; i < jobs.length; i++) {
+        console.log(`${i+1}. ${jobs[i]}`)
+        console.log('--------------------------------------------------------------------------------------------------------')
+    }
 
 
-    return $
+    await browser.close();
 
 
 
 }
+
+runDice()
 
 module.exports = runDice
