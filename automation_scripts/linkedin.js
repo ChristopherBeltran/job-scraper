18d6eff1b1e30ad9bf39a125cdd130ce21666f1f
cdbeltran <chris@leadsherpa.com>
2021-03-21 13:51:55 -0600

    Adding updates to linkedin selectors, upgraded puppeteer to latest

--- automation_scripts/linkedin.js
+++ automation_scripts/linkedin.js
@@ -15,15 +15,15 @@ const runLinkedIn = async (jobTitle, jobLocation, datePosted, sortBy) => {
     const client = await page.target().createCDPSession();
     await client.send('Network.clearBrowserCookies');
     await client.send('Network.clearBrowserCache');
-    await page.waitFor(3000)
+    await page.waitForTimeout(3000)
     await page.goto('https://www.linkedin.com/')
-    await page.waitFor(8000)
+    await page.waitForTimeout(8000)
     await page.goto(`${url}?keywords=${jobTitle}&location=${jobLocation}`);
-    await page.waitFor(8000)
+    await page.waitForTimeout(8000)
 
     //No Past 3 days option on LinkedIn
-    const pastDay = '#TIME_POSTED-dropdown > fieldset > div.filter-list > ul > li:nth-child(1) > label'
-    const pastSevenDays = '#TIME_POSTED-dropdown > fieldset > div.filter-list > ul > li:nth-child(2) > label'
+    const pastDay = '#TIME_POSTED_RANGE-0'
+    const pastSevenDays = '#TIME_POSTED_RANGE-1'
 
     var postedDate
 
@@ -42,24 +42,24 @@ const runLinkedIn = async (jobTitle, jobLocation, datePosted, sortBy) => {
             break;
     }
     //change search to filter by last 3 days
+    const timeDropdownButton = '[data-tracking-control-name=public_jobs_TIME_POSTED_RANGE-dropdown]'
+    const timeDropdownSubmit = '[data-tracking-control-name=f_TPR-done-btn]'
     await page.content()
-    const theurl = await page.url()
-    console.log(theurl)
-    await page.waitForSelector('body > header > section > form > ul > li:nth-child(2) > div > button')
-    await page.click('body > header > section > form > ul > li:nth-child(2) > div > button')
+    await page.waitForSelector(timeDropdownButton)
+    await page.click(timeDropdownButton)
     await page.click(`${postedDate}`)
-    await page.click('#TIME_POSTED-dropdown > fieldset > div.filter-button-dropdown__dropdown-actions > button')
-    await page.waitFor(5000);
+    await page.click(timeDropdownSubmit)
+    await page.waitForTimeout(5000);
 
     var resultsUrl = await page.url()
 
     if (sortBy === 'Most Recent') {
         //change search to filter by most recent
         await page.goto(`${resultsUrl}&sortBy=DD`)
-        await page.waitFor(6000)
+        await page.waitForTimeout(6000)
     } else if (sortBy === 'Most Relevant') {
         await page.goto(`${resultsUrl}&sortBy=R`)
-        await page.waitFor(6000)
+        await page.waitForTimeout(6000)
     }
 
     const content = await page.content();
